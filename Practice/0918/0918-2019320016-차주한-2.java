10462a2557f1a9bedd8bdc965e1ceff4743caa4b
JuHan <hanchaa@gmail.com>
2019-09-18 21:11:37 +0900

    20190918

---
+++ Practice/0918/0918-2019320016-차주한-2.java
@@ -0,0 +1,13 @@
+package io.github.hanchaa;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String s = "Hello World!";
+        System.out.println(s.length());
+        System.out.println(s.charAt(6));
+        System.out.println(s.toLowerCase());
+    }
+}
---
+++ Practice/0918/0918-2019320016-차주한-5.java
@@ -0,0 +1,14 @@
+package io.github.hanchaa;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        final double PI = 3.14;
+        int r = 10;
+        double area = PI * r * r;
+
+        System.out.println("원의 면적 = " + area);
+    }
+}
