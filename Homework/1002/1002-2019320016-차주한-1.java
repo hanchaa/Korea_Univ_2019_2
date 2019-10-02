f863dcfdcb51a996ffe50b99ae19a050cf62f018
hanchaa <hanchaa@gmail.com>
2019-10-02 22:00:20 +0900

    20191002

--- Homework/1002/1002-2019320016-차주한-1.java
+++ Homework/1002/1002-2019320016-차주한-1.java
@@ -3,16 +3,33 @@ package io.github.hanchaa;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int Class, Student;
+        double sum = 0;
+
         System.out.print("반 수 입력: ");
-        int c = sc.nextInt();
+        Class = sc.nextInt();
 
         System.out.print("반별 학생 수 입력: ");
-        int student = sc.nextInt();
+        Student = sc.nextInt();
 
-        for()
+        int[][] score = new int[Class * Student][2];
+
+        for (int i = 0; i < Class; i++) {
+            for (int j = 0; j < Student; j++) {
+                System.out.print("\n국어 점수 입력: ");
+                int ko = sc.nextInt();
+
+                System.out.print("영어 점수 입력: ");
+                int en = sc.nextInt();
+
+                System.out.printf("합 = %d, 평균 = %.2f\n", ko + en, (double) (ko + en) / 2);
+                sum += ko + en;
+            }
+        }
+
+        System.out.printf("\n총합 = %.0f, 과목당평균 = %.2f\n", sum, sum / (2 * Class * Student));
     }
 }
