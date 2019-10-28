ec13c6bf97607598e6bb0d6296f273ebf179ad1c
hanchaa <hanchaa@gmail.com>
2019-10-28 20:24:51 +0900

    20191023

---
+++ Practice/1023/1023-2019320016-차주한-8-1.java
@@ -0,0 +1,11 @@
+package io.github.hanchaa;
+
+public class Main {
+    public static void main(String[] args) {
+        Point p = new Point(1, 2);
+        p.showPoint();
+
+        ColorPoint cp = new ColorPoint(3, 4, "red");
+        cp.showColorPoint();
+    }
+}
